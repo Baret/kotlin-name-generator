11a8bb9d76fb9f1853267672d627c8caee83ad2b
Baret <450955+Baret@users.noreply.github.com>
2023-11-28 21:49:08 +0100

    extended AutoResettingNameGeneratorTest and SimpleWordListTest

--- kotlin-name-generator/src/test/kotlin/de/gleex/kng/wordlist/SimpleWordListTest.kt
+++ kotlin-name-generator/src/test/kotlin/de/gleex/kng/wordlist/SimpleWordListTest.kt
@@ -3,6 +3,8 @@ package de.gleex.kng.wordlist
 import de.gleex.kng.api.WordList
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.equals.shouldNotBeEqual
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.beInstanceOf
@@ -24,6 +26,25 @@ class SimpleWordListTest: WordSpec() {
                 SimpleWordList(listOf("a", "b", "c")).size shouldBe 3
                 SimpleWordList(listOf("abcd")).size shouldBe 1
                 SimpleWordList(listOf("a", "b", "c", "d", "e", "f", "g", "h")).size shouldBe 8
+            }
+        }
+
+        "SimpleWordLists" should {
+            "use the hashCode of the underlying list" {
+                val theList = listOf("foo", "bar", "baz")
+                SimpleWordList(theList).hashCode() shouldBe theList.hashCode()
+            }
+
+            "be equal when created with the same list" {
+                val theList = listOf("foo", "bar", "baz")
+                val first = SimpleWordList(theList)
+                val second = SimpleWordList(theList)
+
+                first shouldBeEqual second
+                first shouldBeEqual first
+                second shouldBeEqual second
+                first shouldNotBeEqual theList
+                first shouldNotBeEqual SimpleWordList(theList + "another")
             }
         }
     }
