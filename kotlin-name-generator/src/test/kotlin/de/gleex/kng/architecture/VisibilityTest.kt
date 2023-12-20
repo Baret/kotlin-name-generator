c6bc6450ccaf21671997af97003fefe740a449b0
Baret <450955+Baret@users.noreply.github.com>
2023-12-20 21:36:20 +0100

    removed konsist, built constructor visibility test

--- kotlin-name-generator/src/test/kotlin/de/gleex/kng/architecture/VisibilityTest.kt
+++ kotlin-name-generator/src/test/kotlin/de/gleex/kng/architecture/VisibilityTest.kt
@@ -1,27 +1,29 @@
 package de.gleex.kng.architecture
 
-import com.lemonappdev.konsist.api.Konsist
-import com.lemonappdev.konsist.api.verify.assertTrue
+import com.tngtech.archunit.core.domain.JavaMember.Predicates.declaredIn
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.constructors
 import de.gleex.kng.api.NameGenerator
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.mpp.bestName
 
 private val log = KotlinLogging.logger {  }
 
+@AnalyzeClasses(packages = ["de.gleex.kng"], importOptions = [ImportOption.DoNotIncludeTests::class])
 class VisibilityTest: WordSpec() {
-    init {
-        "Name generator implementations" should {
-            "be internal" {
-                log.info { "Project path: ${Konsist.projectRootPath}" }
-                Konsist.scopeFromProject()
-                    .print()
-                    .classes()
-                    .also { log.info { "Filtering ${it.size} classes for implementations of ${NameGenerator::class.bestName()}" } }
-                    .filter { it.hasInterfaceWithName(NameGenerator::class.bestName()) }
-                    .also { log.info { "Found ${it.size} classes implementing the interface" } }
-                    .assertTrue(testName = this.testCase.name.testName) { it.hasInternalModifier }
-            }
-        }
-    }
+    @ArchTest
+    val constructorsOnlyInBuilder = constructors()
+        .that()
+        .areDeclaredInClassesThat().implement(NameGenerator::class.java)
+        .should()
+        .onlyBeCalled()
+        .byMethodsThat(declaredIn("de.gleex.kng.generators.NameGenerators"))
+        .orShould()
+        .onlyBeCalled()
+        .byClassesThat().implement(NameGenerator::class.java)
+        .orShould()
+        .onlyBeCalled()
+        .byClassesThat().haveFullyQualifiedName("de.gleex.kng.generators.ExtensionsKt")
 }
